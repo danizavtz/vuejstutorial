514d6324e0fbe295c9636bc8904fb33023f8e284
daniel.pires <daniel.pires@contabilizei.com.br>
2018-12-09 19:28:44 -0200

    First commit, configuração inicial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vuejstutorial
 vuejs express nodejs
+Esse é um tutorial para aprendizado de vuejs, integrado com um projeto de desenvolvimento de uma api. Utilizando mongodb express e vuewjs.
+Link para o tutorial:
+https://www.techiediaries.com/vue-js-express-tutorial/
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+/* load dotenv */
+require('dotenv').config()
+
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
+
+module.exports = app;
